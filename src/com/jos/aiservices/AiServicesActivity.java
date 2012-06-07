ef6f840b4e81b3434719f3e9c99a344940d0c1c8
Josmas Flores <josmasflores@gmail.com>
2012-06-07 17:34:29 +0100

    adds a preferences file and a menu item to interact with the preferences

--- src/com/jos/aiservices/AiServicesActivity.java
+++ src/com/jos/aiservices/AiServicesActivity.java
@@ -1,8 +1,12 @@
 package com.jos.aiservices;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class AiServicesActivity extends Activity {
 
@@ -13,5 +17,22 @@ public class AiServicesActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		Log.d(TAG, "AiServicesActivity created");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.itemPrefs:
+			startActivity(new Intent(this, PrefsActivity.class));
+			break;
+		}
+		return true;
 	}
 }
