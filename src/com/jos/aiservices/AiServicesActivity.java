c874df29fc63ced80a53f0359debb02598735a4a
Josmas Flores <josmasflores@gmail.com>
2012-06-07 19:41:38 +0100

    hooks up the prefs callback on the AiServices activity to modify the alarm depending on the settings saved

--- src/com/jos/aiservices/AiServicesActivity.java
+++ src/com/jos/aiservices/AiServicesActivity.java
@@ -1,22 +1,34 @@
 package com.jos.aiservices;
 
+import com.jos.aiservices.services.ReadFromWebService;
+
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class AiServicesActivity extends Activity {
+public class AiServicesActivity extends Activity implements OnSharedPreferenceChangeListener {
 
 	private static final String TAG = "AiServicesActivity";
+	private SharedPreferences prefs;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		Log.d(TAG, "AiServicesActivity created");
+		
+		prefs = PreferenceManager.getDefaultSharedPreferences(this); 
+		prefs.registerOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
@@ -34,5 +46,21 @@ public class AiServicesActivity extends Activity {
 			break;
 		}
 		return true;
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		long interval = new Long( prefs.getString("webinterval", "300000") ).longValue();
+        Log.d(TAG, "Boot up Receiver was called: onReceive : setting an alarm every " + interval + " minutes (pending intent)");
+        
+        AlarmManager am = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pi = PendingIntent.getService(this, 0, new Intent(this, ReadFromWebService.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        //TODO If inexact repeating is called with a non-defined alarm interval then it's like calling setRepeating <-- convert intervals into alarms
+        if (interval == 0l)
+        	am.cancel(pi);
+        else
+        	am.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + interval, interval, pi);
+		
 	}
 }
--- src/com/jos/aiservices/bootup/BootUpReceiver.java
+++ src/com/jos/aiservices/bootup/BootUpReceiver.java
@@ -5,20 +5,27 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import com.jos.aiservices.services.ReadFromWebService;
 
 public class BootUpReceiver extends BroadcastReceiver {
 	
 	private static final String TAG = "BootUpReceiver";
+	private SharedPreferences prefs;
 
 	@Override
-	public void onReceive(Context context, Intent intent) {	
-        Log.d(TAG, "Boot up Receiver was called: onReceive : setting an alarm every 15 minutes (pending intent)");
-        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        PendingIntent pi = PendingIntent.getService(context, 0, new Intent(context, ReadFromWebService.class), PendingIntent.FLAG_UPDATE_CURRENT);
-        am.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + AlarmManager.INTERVAL_FIFTEEN_MINUTES, AlarmManager.INTERVAL_FIFTEEN_MINUTES, pi);
+	public void onReceive(Context context, Intent intent) {
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        long interval = new Long( prefs.getString("webinterval", "300000") ).longValue();
         
+        if (interval != 0l){
+        	Log.d(TAG, "Boot up Receiver was called: onReceive : setting an alarm every " + interval + " minutes (pending intent)");
+            AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            PendingIntent pi = PendingIntent.getService(context, 0, new Intent(context, ReadFromWebService.class), PendingIntent.FLAG_UPDATE_CURRENT);
+            am.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + interval, interval, pi);        	
+        }
 	}
 
 }
