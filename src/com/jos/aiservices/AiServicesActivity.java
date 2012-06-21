c2fd331e6e2a2a2bd0c26e2238b99e28ec3661b7
Josmas Flores <josmasflores@gmail.com>
2012-06-21 20:16:27 +0100

    traslated the menu from an inflated xml file (deleted) to a pure Java menu. Have to use pure Java in App Inventor

--- src/com/jos/aiservices/AiServicesActivity.java
+++ src/com/jos/aiservices/AiServicesActivity.java
@@ -4,8 +4,10 @@ import com.jos.aiservices.services.ReadFromWebService;
 
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -13,8 +15,8 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 
 /**
  * This is the main activity and it does nothing other than showing a message
@@ -41,20 +43,64 @@ public class AiServicesActivity extends Activity implements OnSharedPreferenceCh
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.menu, menu);
-		return true;
-	}
+    addPrefsButtonToMenu(menu);
+    addExitButtonToMenu(menu);
+    return true;
+  }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.itemPrefs:
-			startActivity(new Intent(this, PrefsActivity.class));
-			break;
-		}
-		return true;
-	}
+  public void addPrefsButtonToMenu(Menu menu) {
+    MenuItem stopApplicationItem = menu.add(Menu.NONE, Menu.NONE, Menu.FIRST,
+    "Prefs")
+    .setOnMenuItemClickListener(new OnMenuItemClickListener() {
+      public boolean onMenuItemClick(MenuItem item) {
+        startPrefsActivity();
+        return true;
+      }
+    });
+    stopApplicationItem.setIcon(android.R.drawable.ic_menu_preferences);
+  }
+  
+  private void startPrefsActivity() {
+    startActivity(new Intent(this, PrefsActivity.class));
+  }
+  
+  public void addExitButtonToMenu(Menu menu) {
+    MenuItem stopApplicationItem = menu.add(Menu.NONE, Menu.NONE, Menu.FIRST,
+    "Stop this application")
+    .setOnMenuItemClickListener(new OnMenuItemClickListener() {
+      public boolean onMenuItemClick(MenuItem item) {
+        showExitApplicationNotification();
+        return true;
+      }
+    });
+    stopApplicationItem.setIcon(android.R.drawable.ic_menu_close_clear_cancel);
+  }
+
+  private void showExitApplicationNotification() {
+    AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+    alertDialog.setTitle("Stop application?");
+    // prevents the user from escaping the dialog by hitting the Back button
+    alertDialog.setCancelable(false);
+    alertDialog.setMessage("Stop this application and exit? You'll need to relaunch " +
+    "the application to use it again.");
+    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Stop and exit",
+        new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface dialog, int which) {
+        // I know that this is frowned upon in Android circles but I really think that it's
+        // confusing to users if the exit button doesn't really stop everything, including other
+        // forms in the app (when we support them), non-UI threads, etc.  We might need to be
+        // careful about this is we ever support services that start up on boot (since it might
+        // mean that the only way to restart that service) is to reboot but that's a long way off.
+        System.exit(0); //TODO Copied this from a different method in the Form class
+      }});
+    alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Don't stop",
+        new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface dialog, int which) {
+        // nothing to do here
+      }
+    });
+    alertDialog.show();
+  }
 
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
